a6a76f023b731016a9572ed4ad92d7c5029e60d8
Juan Manuel Francisco Mendoza <juanma98117@alumnos.ilerna.com>
2025-12-01 17:37:20 +0100

    Añadir comentario local de autor

---
+++ ConversorUnidades.java
@@ -0,0 +1,17 @@
+public class ConversorUnidades {
+
+// Contribución de Juan Pérez: Módulo de temperatura.
+
+// Autor: Juan Manuel Francisco Mendoza - Cambio local
+
+
+    /**
+     * Convierte grados Celsius a Fahrenheit.
+     * Fórmula: F = (C × 9/5) + 32
+     */
+    public static double celsiusAFahrenheit(double celsius) {
+        return (celsius * 9.0 / 5.0) + 32;
+    }
+
+    // Puedes añadir otras funciones de conversión aquí
+}
